1b34f75dbfc12e21234cd69e0f1455c69c81fd0c
thana <thana.j@gmail.com>
2017-08-03 14:05:38 +0700

    test code ship2

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
       class="hidden-sm-and-up"></v-toolbar-side-icon>
      <v-toolbar-title>
       <router-link to="/" tag="span" style="cursor: pointer">
-       Meetup TNT
+       Meetup @ thana
       </router-link>
      </v-toolbar-title>
      <v-spacer></v-spacer>
