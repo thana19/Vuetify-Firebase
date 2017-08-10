dac43ddbf51fbb63629f92cf9b98111c359e49ac
thana <thana.j@gmail.com>
2017-08-10 15:50:01 +0700

    change firebase project

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
       class="hidden-sm-and-up"></v-toolbar-side-icon>
      <v-toolbar-title>
       <router-link to="/" tag="span" style="cursor: pointer">
-       Meetup@Thana
+       Meetups Workshop
       </router-link>
      </v-toolbar-title>
      <v-spacer></v-spacer>
